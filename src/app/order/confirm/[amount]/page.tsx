d158aa5114c192a89d101de33439f40452758081
RJdeck <rockchen4@gmail.com>
2024-01-30 14:25:20 +0800

    fix: pass order price

--- src/app/order/confirm/[amount]/page.tsx
+++ src/app/order/confirm/[amount]/page.tsx
@@ -6,8 +6,9 @@ export const metadata = {
   title: "Payment Confirmation",
 };
 
-export default async function page() {
-  const session = await get_eft_session("0.1");
+export default async function page({ params }: any) {
+  const { amount } = params
+  const session = await get_eft_session(amount);
   const eft_url = (process.env.NEXT_PUBLIC_EFT_BASE_URL + session) as string;
 
   return <PaymentConfirmation url={eft_url} />;
